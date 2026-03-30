4a021fbd079581efe6590f1c72c732e20bb5b575
Sourashis07 <sourashis7ps7sabud@gmail.com>
2026-03-30 14:52:35 +0530

    initial

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,15 @@
+/**
+ * Book My Stay App
+ * Hotel Booking Management System
+ * @author Sourashis
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Book My Stay");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version 1.0");
+        System.out.println("Welcome to the Application");
+    }
+}
